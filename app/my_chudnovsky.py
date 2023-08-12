b9934c0ca4dcbd6df9850b89ee8fa0973d904ec6
heko <91366057+hkohko@users.noreply.github.com>
2023-08-12 17:00:00 +0700

    created Pi object

--- app/my_chudnovsky.py
+++ app/my_chudnovsky.py
@@ -1,6 +1,6 @@
 import gmpy2
 from math import ceil
-from enum import Enum
+from dataclasses import dataclass
 from gmpy2 import mpz, fac
 from time import perf_counter
 from tqdm import tqdm
@@ -12,75 +12,64 @@ all these prime swing factorials are slower than gmpy2.fac?
 # from primeswing_factorial.PrimeSwingFactorialPy import primeswing_factorial as fac
 # from primeswing_factorial.Andreas_prime_swing import divide_swing_and_conquer as fac
 
+class Pi:
+    def __init__(self, n: int):
+        self.set_prec(n)
+        self.digit = n
+        self.step = ceil(n / 14)
+        self.A = mpz(13591409)
+        self.B = mpz(545140134)
+        self.C = mpz(-262537412640768000)
+        self.D = mpz(426880) * gmpy2.sqrt(10005)
 
-def gmp_prec(n: int):
-    n = n + 5  # use higher than requrested precision
-    return int(ceil(n * gmpy2.log(10) / gmpy2.log(2))) + 1
-
-
-def set_prec(n):
-    gmpy2.get_context().precision = gmp_prec(n)
-
-
-class Constants(Enum):
-    A = mpz(13591409)
-    B = mpz(545140134)
-    C = mpz(-262537412640768000)
-    D = mpz(426880) * gmpy2.sqrt(10005)
+    def gmp_prec(self, n: int):
+        n = n + 5  # use higher than requrested precision
+        return int(ceil(n * gmpy2.log(10) / gmpy2.log(2))) + 1
 
 
-def m_q(step: int) -> mpz:
-    nom = fac(6 * mpz(step))
-    b = fac(3 * mpz(step))
-    c = pow(fac(mpz(step)), 3)
-    denom = b * c
-    result = nom // denom
-    return result
+    def set_prec(self, n):
+        gmpy2.get_context().precision = self.gmp_prec(n)
+        
+
+    def m_q(self, step: int) -> mpz:
+        nom = fac(6 * mpz(step))
+        b = fac(3 * mpz(step))
+        c = pow(fac(mpz(step)), 3)
+        denom = b * c
+        result = nom // denom
+        return result
 
 
-def l_q(step: int) -> mpz:
-    a = Constants.B.value * step
-    return a + Constants.A.value
+    def l_q(self, step: int) -> mpz:
+        a = self.B * step
+        return a + self.A
 
 
-def x_q(step: int) -> mpz:
-    a = pow(Constants.C.value, step)
-    return a
+    def x_q(self, step: int) -> mpz:
+        a = pow(self.C, step)
+        return a
 
 
-def division(steps: int) -> gmpy2.mpfr:
-    for step in range(steps):
-        result = (m_q(step) * l_q(step)) / x_q(step)
-        yield result
+    def division(self) -> gmpy2.mpfr:
+        for step in range(self.step):
+            result = (self.m_q(step) * self.l_q(step)) / self.x_q(step)
+            yield result
 
 
-def summation(step: int) -> gmpy2.mpfr:
-    result = 0
-    for i in tqdm(division(step)):
-        result = result + i
-    return result
+    def summation(self) -> gmpy2.mpfr:
+        result = 0
+        for i in tqdm(self.division()):
+            result = result + i
+        return result
+
+    def generate(self):
+        return self.D / self.summation()
+
+    def __repr__(self):
+        a = f"{self.generate():.{self.digit}Df}"
+        return a[-10:]
+    
+n = 50
+print(Pi(n))
 
 
-def pi(digit: int, step: int = 4):
-    a = f"{Constants.D.value / summation(step):.{digit}Df}"
-    return a
-
-
-if __name__ == "__main__":
-    while True:
-        n = int(input("Number of digits : "))
-        if gmp_prec(n) < int(gmpy2.get_max_precision()):
-            gmpy2.get_context().precision = gmp_prec(n)
-            start = perf_counter()
-            print(f"expected iteration: {ceil(n / 14)}")
-            class Constants(Enum):
-                A = mpz(13591409)
-                B = mpz(545140134)
-                C = mpz(-262537412640768000)
-                D = mpz(426880) * gmpy2.sqrt(10005)
-
-            print(pi(n, ceil(n / 14))[-10:])
-            end = perf_counter()
-            print(f"{end - start:.2f}s")
-        else:
-            print("Exceeded max precision")
